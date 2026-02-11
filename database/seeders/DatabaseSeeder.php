f7e42c798c65ed30d2db2c37be853edf0404770f
Emerson <opaemerson@git.com>
2026-02-11 12:03:10 -0300

    adaptacoes de modulos e arquivos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Supplier;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,8 +14,6 @@ class DatabaseSeeder extends Seeder
             FunctionalitiesTableSeeder::class,
             SuperadminUserSeeder::class,
             AdminUserSeeder::class,
-            CategorySeeder::class,
-            SupplierSeeder::class,
         ]);
     }
 }
